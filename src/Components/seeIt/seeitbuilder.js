279d5a18942e2eb361e798f2cc87dedec484c497
Havey3 <45610955+Havey3@users.noreply.github.com>
2019-04-02 09:37:35 -0400

    commiting before starting on edit delete.  changed a little css on the add form

--- src/Components/seeIt/seeitbuilder.js
+++ src/Components/seeIt/seeitbuilder.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import "./seeit.css"
 
 export default class SeeItForm extends Component {
     // Set State
@@ -37,15 +38,15 @@ export default class SeeItForm extends Component {
     render() {
         return (
             <React.Fragment>
-                <form>
+                <form className="seeitform">
                     <div className="form-group">
-                        <label htmlFor="exampleFormControlInput1">Movie Title</label>
+                        <label htmlFor="exampleFormControlInput1">Add Movie</label>
                         <input type="text" className="form-control" onChange={this.handleFieldChange} id="movieTitle" placeholder="Movie Title"></input>
                         <input type="text" className="form-control" onChange={this.handleFieldChange} id="movieDirector" placeholder="Movie Director"></input>
                         <input type="img" className="form-control" onChange={this.handleFieldChange} id="movieImg" placeholder="Movie Image"></input>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="exampleFormControlSelect1">Example select</label>
+                        <label htmlFor="exampleFormControlSelect1">Rank</label>
                         <select className="form-control" onChange={this.handleFieldChange} id="movieRank">
                             <option id="movieRank">1</option>
                             <option id="movieRank">2</option>
@@ -55,7 +56,7 @@ export default class SeeItForm extends Component {
                         </select>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
+                        <label htmlFor="exampleFormControlTextarea1">Notes</label>
                         <textarea className="form-control" onChange={this.handleFieldChange} id="movieNotes" rows="3"></textarea>
                     </div>
                     <button
